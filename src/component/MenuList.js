cf39c7da247204fb065bb3bab652b0929522e330
archanak55201 <55021100+archanak55201@users.noreply.github.com>
2023-12-07 20:53:28 +0530

    Update MenuList.js

--- src/component/MenuList.js
+++ src/component/MenuList.js
@@ -6,8 +6,8 @@ const MenuList=({value,closemenu})=> {
   return (
     <div style={{display:`${value}`}} className='menu'>
           <div className='menu-name'><NavLink to={'/home'} onClick={()=>{closemenu()}}>Home</NavLink></div>
-          <div className='menu-name'><NavLink to={'/inquiry'} onClick={()=>{closemenu()}}>Bulk Inquires</NavLink></div>
-          <div className='menu-name'><NavLink to={'/ourStory'} onClick={()=>{closemenu()}}>Our Stories</NavLink></div>
+          <div className='menu-name'><NavLink to={'/inquiry'} onClick={()=>{closemenu()}}>Inquiries</NavLink></div>
+          <div className='menu-name'><NavLink to={'/ourStory'} onClick={()=>{closemenu()}}>Story</NavLink></div>
           <div className='menu-name'><NavLink to={'/policies'} onClick={()=>{closemenu()}}>Return Policies</NavLink></div>
           <div className='menu-name'><NavLink to={'/chat-with-us'} onClick={()=>{closemenu()}}>Chat With Us</NavLink></div>
     </div>
