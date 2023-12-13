0a1e31b8c2c82a0d964b819f7bbd7ce1aab81171
archanak55201 <55021100+archanak55201@users.noreply.github.com>
2023-12-14 01:16:20 +0530

    Add files via upload

--- src/Pages/SingleProduct.js
+++ src/Pages/SingleProduct.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useLocation } from 'react-router-dom';
+import { FaWhatsapp } from "react-icons/fa";
 import { useProductContext } from '../ProductContext';
 import './productpage.css';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 function SingleProduct() {
-  
+    const [count,setCount] = useState(1);
+    const [message,setMessage] = useState("");
     const {product} = useProductContext();
+
+    const phoneNumber = '9407464630';
+        
+    console.log(product.name+","+count);
    
     console.log(product);
     const url = "./../images/";
@@ -33,6 +39,13 @@ function SingleProduct() {
         maxWidth: '100%',
         height: 'auto',
       };
+
+      const handlePurchase=()=>{
+        setMessage(product.name+","+count);
+        const encodedMessage = encodeURIComponent(message);
+        window.location.href = `whatsapp://send?phone=${phoneNumber}&text=${encodedMessage}`;
+      
+      }
   return (
     <div className='product-detail' >
        
@@ -47,8 +60,39 @@ function SingleProduct() {
         </Slider>
         </div>
       
-        <div>
-            {product.name}
+        <div className='product-description'>
+            <div className='product-title'>{product.name}</div>
+            <div className='product-price'>$49.00</div>
+            <div>
+                <h3>Key Features</h3>
+                <ul>
+                    <li>Outer shell - 100% GOTS Certified Organic Cotton</li>
+                    <li>Filling - Polyfilling</li>
+                    <li>Non Toxi dyes used and hence it is chew friendly</li>
+                    <li>Dimensions - </li>
+                    <li>Bhakur Plushies teach kids important skills.Children often express emotions and thoughts while playing with plushies.</li>
+                    
+                </ul>
+            </div>
+            <div className='quantity'>
+                <div>Qunatity</div>
+                <div className='quantity-count'>
+                <button onClick={()=>{setCount(count-1)}}>-</button>
+                <span>{count}</span>
+                <button onClick={()=>{setCount(count+1)}}>+</button>
+                </div>
+            </div>
+            <div className='purchase-btn'>
+                
+                <button onClick={handlePurchase}>
+                    <div style={{color:"white",display:"flex",alignItems:"center"}}><FaWhatsapp style={{fontSize:"30px"}}/></div>
+                    <a className='btn-name' href={`whatsapp://send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`} >
+                    Click to Purchase on Whatsapp
+                    </a>
+                   
+                </button>
+            </div>
+            
         </div>
         
     </div>
