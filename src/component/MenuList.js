168510ab59326b7c26c5f711e7e383542659a4f1
archanak55201 <55021100+archanak55201@users.noreply.github.com>
2024-02-19 13:53:51 +0530

    Add files via upload

--- src/component/MenuList.js
+++ src/component/MenuList.js
@@ -4,11 +4,15 @@ import { NavLink} from 'react-router-dom';
 const MenuList=({value,closemenu})=> {
     
   return (
-    <div style={{display:`${value}`}} className='menu'>
+    <div style={{display:`${value}`}} className='menu' >
           <div className='menu-name'><NavLink to={'/home'} onClick={()=>{closemenu()}}>Home</NavLink></div>
-          <div className='menu-name'><NavLink to={'/inquiry'} onClick={()=>{closemenu()}}> Inquiries</NavLink></div>
+          
           <div className='menu-name'><NavLink to={'/ourStory'} onClick={()=>{closemenu()}}>Story</NavLink></div>
+
+          <div className='menu-name'><NavLink to={'/founder'} onClick={()=>{closemenu()}}> Our Founders</NavLink></div>
+
           <div className='menu-name'><NavLink to={'/policies'} onClick={()=>{closemenu()}}>Return Policies</NavLink></div>
+          
           <div className='menu-name'><NavLink to={'/chat-with-us'} onClick={()=>{closemenu()}}>Chat With Us</NavLink></div>
     </div>
   )
